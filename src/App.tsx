9a881290c08cc45a52f805e436d46cfd732418a2
yuanda <6232147-yuandahanif@users.noreply.gitlab.com>
2023-04-02 16:30:46 +0700

    feat: :tada: init config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="mx-auto flex min-h-screen max-w-screen-2xl border-x  bg-[#242424] shadow-md">
+      <aside>ini daftar</aside>
+      <main>ini main</main>
+    </div>
+  );
+}
+
+export default App;
